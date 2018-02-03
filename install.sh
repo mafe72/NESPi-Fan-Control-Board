7c9c576843be2455eebd0d80e5cdf67847abdad3
mafe72 <mafe72@users.noreply.github.com>
2018-02-03 13:33:28 -0600

    Initial commit

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#Step 1) Check if root--------------------------------------
+if [[ $EUID -ne 0 ]]; then
+   echo "Please execute script as root." 
+   exit 1
+fi
+#-----------------------------------------------------------
+
+#Step 2) Update repository----------------------------------
+sudo apt-get update -y
+#-----------------------------------------------------------
+
+#Step 3) Install gpiozero module----------------------------
+sudo apt-get install -y python-dev python-pip python-gpiozero
+sudo pip install psutil pyserial
+#-----------------------------------------------------------
+
+#Step 4) Download Python script-----------------------------
+cd /opt/
+sudo mkdir RetroFlag
+cd /opt/RetroFlag
+script=fan_ctrl.py
+
+if [ -e $script ];
+	then
+		echo "Script fan_ctrl.py already exists. Updating..."
+		rm $script
+		wget "https://raw.githubusercontent.com/mafe72/Retroflag-NESPi-Fan-Control-Board/master/scripts/fan_ctrl.py"
+		echo "Update complete."
+	else
+		wget "https://raw.githubusercontent.com/mafe72/Retroflag-NESPi-Fan-Control-Board/master/scripts/fan_ctrl.py"
+fi
+#-----------------------------------------------------------
+
+#Step 5) Enable Python script to run on start up------------
+cd /etc/
+RC=rc.local
+
+#Cleaning deprecated configuration----
+if grep -q "sudo python3 \/opt\/RetroFlag\/fan_ctrl.py \&" "$RC";
+        then
+               sed -i '/sudo python3 \/opt\/RetroFlag\/fan_ctrl.py \&/c\' "$RC";
+fi
+
+#Adding new configuration----------- 
+if grep -q "sudo python \/opt\/RetroFlag\/fan_ctrl.py \&" "$RC";
+	then
+		echo "File /etc/rc.local already configured. Doing nothing."
+	else
+		sed -i -e "s/^exit 0/sudo python \/opt\/RetroFlag\/fan_ctrl.py \&\n&/g" "$RC"
+		echo "File /etc/rc.local configured."
+fi
+#-----------------------------------------------------------
+
+#Step 6) Reboot to apply changes----------------------------
+echo "Retroflag NESPi Fan Control Board installation done. Will now reboot after 3 seconds."
+sleep 4
+sudo reboot
+#-----------------------------------------------------------
+
---
+++ scripts/fan_ctrl.py
@@ -0,0 +1,49 @@
+#####################################
+# RetroFlag NESPi Control Board Script
+#####################################
+# Hardware:
+# Board by Eladio Martinez
+# http://mini-mods.com
+#
+#####################################
+# Wiring:
+#  GPIO 4  Fan on signal (OUTPUT)
+#
+#####################################
+#  Required python libraries
+#  sudo apt-get update
+#  sudo apt-get install python-dev python-pip python-gpiozero
+#  sudo pip install psutil pyserial
+#
+#####################################
+# Basic Usage:
+#  FAN ON
+#	Fan will turn ON when temperature exceeded 55C
+#  FAN OFF
+#	Fan will turn OFF when temperature under 40C
+
+import RPi.GPIO as GPIO
+import time
+import os
+import socket
+from gpiozero import DigitalOutputDevice
+
+fan = DigitalOutputDevice(4)
+
+#Get CPU Temperature
+def getCPUtemp():
+	res = os.popen('vcgencmd measure_temp').readline()
+	return (res.replace("temp=","").replace("'C\n",""))
+	
+while True:
+	#Fan control
+	#Adjust MIN and MAX TEMP as needed to keep the FAN from kicking
+	#on and off with only a one second loop
+	cpuTemp = int(float(getCPUtemp()))
+	fanOnTemp = 55  #Turn on fan when exceeded
+	fanOffTemp = 40  #Turn off fan when under
+	if cpuTemp >= fanOnTemp:
+		fan.on()
+	if cpuTemp < fanOffTemp:
+		fan.off()
+	time.sleep(0.50)
