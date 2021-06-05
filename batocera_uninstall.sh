95e463ef402d9a349a9bdc14af9d629c59835324
mafe72 <mafe72@users.noreply.github.com>
2021-06-05 17:44:13 -0500

    Add uninstall script compatible with Batocera

---
+++ batocera_uninstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Step 1)make /boot writable---------------------------------
+sleep 2s
+
+mount -o remount, rw /boot
+mount -o remount, rw /
+#-----------------------------------------------------------
+
+#Step 2) Remove Installation directory ---------------------
+rm -r /opt/MiniMods
+sleep 2s
+
+#-----------------------------------------------------------
+
+#Step 3) Remove configuration script ------------
+rm /etc/init.d/S99NESPiFanCTRL
+
+#-----------------------------------------------------------
+
+#Step 4) Reboot to apply changes----------------------------
+echo "NESPi Fan Control Board un-install complete. Will now reboot after 3 seconds."
+sleep 4
+reboot
+#-----------------------------------------------------------
