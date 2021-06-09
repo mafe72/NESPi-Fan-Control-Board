d244432102b3b7d8de1b2134db0e1c6f5fd6129a
mafe72 <mafe72@users.noreply.github.com>
2021-06-09 15:01:35 -0500

    Add install script compatible with RecalBox

--- recalbox_install.sh
+++ recalbox_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SourcePath=https://raw.githubusercontent.com/mafe72/NESPi-Fan-Control-Board/master/scripts
-
 #Step 1)make /boot writable---------------------------------
 sleep 2s
 
@@ -9,45 +7,48 @@ mount -o remount, rw /boot
 mount -o remount, rw /
 #-----------------------------------------------------------
 
+SourcePath=https://raw.githubusercontent.com/mafe72/NESPi-Fan-Control-Board/master/scripts
+script=/opt/MiniMods/fan_ctrl.py
+DIR=/etc/init.d/S99NESPiFanCTRL
+
+#-----------------------------------------------------------
 #Step 2)Download Python script-----------------------------
 mkdir /opt/MiniMods
 sleep 2s
 
-script=/opt/MiniMods/fan_ctrl.py
 if [ -e $script ];
 	then
-		echo "Script fan_ctrl.py already exists. Updating..."
+		echo "Fan control already installed. Updating..."
 		rm $script
 		wget -O  $script "$SourcePath/fan_ctrl-recalbox.py"
-		echo "Update complete."
+		echo "Fan Control update complete."
 	else
 		wget -O  $script "$SourcePath/fan_ctrl-recalbox.py"
-        echo "Download complete."
+        	echo "Fan Control download complete."
 fi
 #-----------------------------------------------------------
 
 sleep 2s
 
 #Step 3) Enable Python script to run on start up------------
-DIR=/etc/init.d/S99NESPiFanCTRL
 
 if grep -q "python $script &" "$DIR";
 	then
-		if [ -x $DIR ];
-			then 
-				echo "Executable S99NESPiFanCTRL already configured. Doing nothing."
-			else
-				chmod +x $DIR
-		fi
+	if [ -x $DIR ];
+	then 
+		echo "Fan Control S99NESPiFanCTRL already configured. Doing nothing."
+	else
+		chmod +x $DIR
+	fi
 	else
 		echo "python $script &" >> $DIR
 		chmod +x $DIR
-		echo "Executable S99NESPiFanCTRL configured."
+		echo "Fan Cotnrol S99NESPiFanCTRL configured."
 fi
 #-----------------------------------------------------------
 
 #Step 4) Reboot to apply changes----------------------------
-echo "NESPi Fan Control Board installation done. Will now reboot after 3 seconds."
+echo "NESPi Fan Control Board installation complete. Rebooting after 3 seconds."
 sleep 3
 reboot
 #-----------------------------------------------------------
