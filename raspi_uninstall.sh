0ddef40a7db4e87d74e62160ca7ad6c0da5a6479
mafe72 <mafe72@users.noreply.github.com>
2021-08-12 12:55:24 -0500

    Add RASPI-Fan Board Unistall script

---
+++ raspi_uninstall.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#Un-install script For Raspbian and RetroPie RASPi-Fan
+
+#Step 1) Check if root--------------------------------------
+if [[ $EUID -ne 0 ]]; then
+   echo "Please execute script as root." 
+   exit 1
+fi
+#-----------------------------------------------------------
+
+DIR=/opt/MiniMods
+RC=/etc/rc.local
+script=/opt/MiniMods/fan_ctrl.py
+
+#-----------------------------------------------------------
+#Step 2) Remove Installation directory ---------------------
+
+sudo rm -r $DIR
+
+#-----------------------------------------------------------
+
+#Step 3) Remove configuration script -------------
+
+#Cleaning deprecated configration files --------------------
+echo Cleaning configration files from rc.local
+
+if grep -q "sudo python3 \/opt\/RetroFlag\/fan_ctrl.py \&" "$RC";
+        then
+               sed -i '/sudo python3 \/opt\/RetroFlag\/fan_ctrl.py \&/c\' "$RC";
+fi
+
+if grep -q "sudo python \/opt\/RetroFlag\/fan_ctrl.py \&" "$RC";
+        then
+               sed -i '/sudo python3 \/opt\/RetroFlag\/fan_ctrl.py \&/c\' "$RC";
+fi
+
+if grep -q "sudo python3 \/opt\/RetroFlag\/fan_ctrl-retroflag.py \&" "$RC";
+        then
+               sed -i '/sudo python3 \/opt\/RetroFlag\/fan_ctrl-retroflag.py \&/c\' "$RC";
+fi
+
+if grep -q "sudo python \/opt\/RetroFlag\/fan_ctrl-retroflag.py \&" "$RC";
+        then
+               sed -i '/sudo python \/opt\/RetroFlag\/fan_ctrl-retroflag.py \&/c\' "$RC";
+fi
+
+if grep -q "sudo python \/opt\/MiniMods\/fan_ctrl.py \&" "$RC";
+        then
+               sed -i '/sudo python \/opt\/MiniMods\/fan_ctrl.py \&/c\' "$RC";
+fi
+
+#-----------------------------------------------------------
+#Step 4) Reboot to apply changes----------------------------
+echo "NESPi Fan Control Board un-install complete. Rebooting after 3 seconds."
+sleep 4
+sudo reboot
+#-----------------------------------------------------------
